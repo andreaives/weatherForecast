1f2dd7b0ec5f9b51944a896ab6cf6ab09a124a6d
andreaives <andreasue3@gmail.com>
2020-10-06 14:55:46 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//global variable of searchedCity;
+// array of searched cities 
+
+
+// search weather function
+//.then will add them on page for todays forecast
+// will be called for this city and another city when typed
+
+//create button function 
+//take in the name of the city  passing in the value of the text box
+
+//forecast function (just like get weather) 
+
+//uv search function: will take in latitude and longitude -- its a different url 
+// passing in latitude and longitude that we got from the api call 
+
+//things that can trip me up
+// getting the url is hard tyler is slacking it 
+//refence 06-13 
+
+//https://api.openweathermap.org/data/2.5/weather?q=${search}&units=imperial&appid= 
+//add api key
+//put city in template literal 
+
+
+
+//uv api call
+//https://api.openweathermap.org/data/2.5/uvi?appid=(api key goes here)&lat=${lat}&lon=${lon}
+
+//source code for image 
+//https://openweathermap.org/img/wn/${weatherIcon}@2x.png 
+// template literal comes from the original api call
+
+//response.data[0]
